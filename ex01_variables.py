dece402fdf9e2dd2062f005dad1fdbf8d1dc4557
Keegan Samuel <keegansamuel47@gmail.com>
2026-06-11 02:59:24 +0100

    Add first lesson about variables and type

---
+++ ex01_variables.py
@@ -0,0 +1,29 @@
+from operator import truediv
+
+name = " keegan"
+age = 26
+height_in_meters = 1.83
+whether_i_am_enjoying_this = True
+print(name)
+print(age)
+print(height_in_meters)
+print(whether_i_am_enjoying_this)
+print(type(name))
+print(type(age))
+print(type(height_in_meters))
+print(type(whether_i_am_enjoying_this))
+
+a = 10
+b =99
+a ,b = b ,a
+print(a)
+print(b)
+
+
+money = 50
+#i understand python execute code line by line , first of all chech the value for the variable money , which is 50
+money = money + 25
+#then move to the next line and did the math instruction takind the value of money as 50, the add it to 25, as written in the code
+money = money - 10
+#then minus 10 from it
+print(money)
