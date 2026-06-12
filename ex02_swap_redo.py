8b9391019055e34731fd045a2f4cc6e64820b91a
Keegan Samuel <keegansamuel47@gmail.com>
2026-06-12 15:46:29 +0100

    homework

---
+++ ex02_swap_redo.py
@@ -0,0 +1,11 @@
+z = 4
+p = 8
+temp = z
+z = p
+p = temp
+print(z)
+print(p)
+
+#the first line gives 55 , because there is "" which is a str python treats it as adding two words together.
+#the second line gave 10 , treats it as addition of two numbers integer.
+#the third line is hahaha, str and int , multiplying the str to the number
