c6dc42f2d6de58b2cb57cac08177d212da8895d7
Keegan Samuel <keegansamuel47@gmail.com>
2026-06-12 17:33:19 +0100

    perfecting how to properly use the f string

---
+++ ex03_fstrings.py
@@ -0,0 +1,14 @@
+name = "keegan"
+age = 26
+height = 1.83
+print(f" my name is {name} and i am {age} years old , my height is {height} in meters.")
+
+item = 4
+cost_per_item = 2.5
+print(f" 4 items cost ${ item * cost_per_item :.2f} in total. ")
+
+my_height_in_meters = 1.83
+print(f"My height is {my_height_in_meters:.1f} meters")
+
+# 555
+print(f"{'5' * 3}")
